3b059141605fbc82d3bb87876056702a84bc1d8d
Tim Veil <tjveil@gmail.com>
2016-12-17 09:25:32 -0500

    adding back commons-codec

---
+++ src/test/java/TestHarness.java
@@ -0,0 +1,13 @@
+import org.apache.hadoop.util.VersionInfo;
+
+/**
+ * Created by tveil on 12/16/16.
+ */
+public class TestHarness {
+
+    public static void main(String[] args) {
+
+        System.out.println(VersionInfo.getVersion());
+
+    }
+}
