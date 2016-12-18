5ecac3a3988cfd2bf88cb097919f804002fcd1d7
Tim Veil <tjveil@gmail.com>
2016-12-17 20:43:27 -0500

    adding back commons-codec

--- src/main/java/Harness.java
+++ src/main/java/Harness.java
@@ -1,13 +1,14 @@
+import org.apache.hadoop.hive.shims.ShimLoader;
 import org.apache.hadoop.util.VersionInfo;
 
-/**
- * Created by tveil on 12/16/16.
- */
-public class TestHarness {
+
+public class Harness {
 
     public static void main(String[] args) {
 
         System.out.println(VersionInfo.getVersion());
+
+        System.out.println(ShimLoader.getMajorVersion());
 
     }
 }
